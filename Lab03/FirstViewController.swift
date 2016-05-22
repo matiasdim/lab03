2edf0ef19d43f86fa20fed31e5758e05613170dc
matiasdim <matiasdim3@gmail.com>
2016-05-21 21:20:12 -0400

    .

--- Lab03/FirstViewController.swift
+++ Lab03/FirstViewController.swift
@@ -6,20 +6,95 @@
 //  Copyright © 2016 Matías Gil Echavarría. All rights reserved.
 //
 
+//http://www.iconbeast.com/free/
+// http://mybaco.deviantart.com/art/Pack-3-Baco-Flurry-Icon-342386574
+
 import UIKit
+import CoreLocation
+import Darwin
+
+// I found this nice and single extension to roound Dubles, here: http://stackoverflow.com/a/28652692/2507118 and I edited it to round Up insted to round Down
+extension Double {
+    var roundTo2:Double {
+        let converter = NSNumberFormatter()
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = NSNumberFormatterStyle.NoStyle
+        formatter.minimumFractionDigits = 2
+        formatter.roundingMode = .RoundUp
+        formatter.maximumFractionDigits = 2
+        if let stringFromDouble =  formatter.stringFromNumber(self) {
+            if let doubleFromString = converter.numberFromString( stringFromDouble ) as? Double {
+                return doubleFromString
+            }
+        }
+        return 0
+    }
+}
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var lat1: DecimalMinusTextField!
+    @IBOutlet weak var lat2: DecimalMinusTextField!
+    @IBOutlet weak var lon1: DecimalMinusTextField!
+    @IBOutlet weak var lon2: DecimalMinusTextField!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var bearingLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.lat1.clearButtonMode = UITextFieldViewMode.WhileEditing
+        self.lat2.clearButtonMode = UITextFieldViewMode.WhileEditing
+        self.lon1.clearButtonMode = UITextFieldViewMode.WhileEditing
+        self.lon2.clearButtonMode = UITextFieldViewMode.WhileEditing
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func calculate(sender: AnyObject) {
+        view.endEditing(true)
+        if (self.lat1 != "" || self.lat2 != "" || self.lon1 != "" || lat2 != "")
+        {
+            let lat1 :CLLocationDegrees = Double(self.lat1.text!)!
+            let lon1 :CLLocationDegrees = Double(self.lon1.text!)!
+            let lat2 :CLLocationDegrees = Double(self.lat2.text!)!
+            let lon2 :CLLocationDegrees = Double(self.lon2.text!)!
 
+
+            let location1 = CLLocation(latitude: lat1, longitude: lon1)
+            let location2 = CLLocation(latitude: lat2, longitude: lon2)
+            
+            let distance :CLLocationDistance = location1.distanceFromLocation(location2)
+            let bearing :Double = location1.bearingToPoint(location2)
+            
+            self.distanceLabel.text = "\((Double(distance) / 1000.0).roundTo2) kilometers"
+            self.bearingLabel.text = "\(Double(bearing).roundTo2 / 1) degrees"
+
+        }
+    }
+    @IBAction func clear(sender: AnyObject) {
+        view.endEditing(true)
+        self.lat1.text = ""
+        self.lat2.text = ""
+        self.lon1.text = ""
+        self.lon2.text = ""
+        self.distanceLabel.text = ""
+        self.bearingLabel.text = ""
+    }
+    
+    
 
 }
 
--- Lab03/SecondViewController.swift
+++ Lab03/SecondViewController.swift
@@ -5,21 +5,80 @@
 //  Created by Matías  Gil Echavarría on 5/19/16.
 //  Copyright © 2016 Matías Gil Echavarría. All rights reserved.
 //
+//  ************************************************
+//  ******** Authors: Matias Gil - Vinvith Mudugonda
+//  ************************************************
 
 import UIKit
+import CoreLocation
+
+// I found this nice and single extension to roound Dubles, here: http://stackoverflow.com/a/28652692/2507118 and I edited it to round Up insted to round Down
+extension Double {
+    var roundTo5:Double {
+        let converter = NSNumberFormatter()
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = NSNumberFormatterStyle.NoStyle
+        formatter.minimumFractionDigits = 5
+        formatter.roundingMode = .RoundUp
+        formatter.maximumFractionDigits = 5
+        if let stringFromDouble =  formatter.stringFromNumber(self) {
+            if let doubleFromString = converter.numberFromString( stringFromDouble ) as? Double {
+                return doubleFromString
+            }
+        }
+        return 0
+    }
+}
+
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var startLatitud: DecimalMinusTextField!
+    @IBOutlet weak var startLongitud: DecimalMinusTextField!
+    @IBOutlet weak var bearing: DecimalMinusTextField!
+    @IBOutlet weak var distance: DecimalMinusTextField!
+    @IBOutlet weak var endpoint: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.startLatitud.clearButtonMode = UITextFieldViewMode.WhileEditing;
+        self.startLongitud.clearButtonMode = UITextFieldViewMode.WhileEditing;
+        self.bearing.clearButtonMode = UITextFieldViewMode.WhileEditing;
+        self.distance.clearButtonMode = UITextFieldViewMode.WhileEditing;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        view.endEditing(true)
+    }
 
+
+    @IBAction func calculate(sender: AnyObject) {
+        view.endEditing(true)
+        let lat1 :CLLocationDegrees = Double(self.startLatitud.text!)!
+        let lon1 :CLLocationDegrees = Double(self.startLongitud.text!)!
+        
+        
+        let location = CLLocation(latitude: lat1, longitude: lon1)
+        
+        let endpoint :CLLocation = location.findingPoint(Double(self.bearing.text!)!, atDistance: Double(self.distance.text!)!)
+        self.endpoint.text = "(\(Double(endpoint.coordinate.latitude).roundTo5),\(Double(endpoint.coordinate.longitude).roundTo5))"
+
+        
+    }
+    @IBAction func clear(sender: AnyObject) {
+        view.endEditing(true)
+        self.startLatitud.text = ""
+        self.startLongitud.text = ""
+        self.bearing.text = ""
+        self.distance.text = ""
+        self.endpoint.text = ""
+    }
 
 }
 
